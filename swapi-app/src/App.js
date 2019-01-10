5ff7c8bc42eaaaa90e5e9288e7a23541fd836798
dsanderson90 <dsanderson3@cnm.edu>
2019-01-10 08:52:43 -0700

    brought in materialize

--- swapi-app/src/App.js
+++ swapi-app/src/App.js
@@ -13,8 +13,28 @@ class App extends Component {
 
   render() {
     return (
-      <div>
-         <h1>Star Wars</h1>
+      <div className="container">
+
+			<div className="row">
+				<div className="col s1 offset-s1">1</div>
+				<div className="col s1">2</div>
+				<div className="col s1">3</div>
+				<div className="col s1">4</div>
+				<div className="col s1">5</div>
+				<div className="col s1">6</div>
+				<div className="col s1">7</div>
+				<div className="col s1">8</div>
+				<div className="col s1">9</div>
+				<div className="col s1">10</div>
+
+			</div>
+         <div className="row">
+            <div className="col s4 offset-s4">
+         <h1 className="text-center">Star Wars</h1>
+         </div>
+
+         </div>
+
          <Characters/>
 
       </div>
